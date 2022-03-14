7e54b0fbaae65d98539bd054ba078a2285b22e53
BIEMAX <tecnico.beilke@gmail.com>
2022-03-13 21:27:33 -0300

    Created new windows

--- FlowerShopWinApp/FrmOrders.cs
+++ FlowerShopWinApp/FrmOrders.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FlowerShopWinApp
 {
-    public partial class Form1 : Form
+    public partial class FrmOrders : Form
     {
-        public Form1()
+        public FrmOrders()
         {
             InitializeComponent();
         }
